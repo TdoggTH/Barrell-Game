b089e2365d8409dcfec334e9987069aa13258195
earblade <tjlknuckles@hotmail.com>
2026-08-16 16:58:46 +0800

    init poisen barrell

--- game-files/Entities/barrell.gd
+++ game-files/Entities/barrell.gd
@@ -8,14 +8,12 @@ var is_picked_up : bool
 var holder : Node3D
 
 
-# got a strong feeling this might bite me in the ass later
-# i think we can fix whatever might get me by making this enum part of a
-# global values list
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print(get_node("./"))
 	health.connect("dies", _barrell_explode)
 	is_picked_up = false
+
 	entities.player.connect("pickup_interactable", _get_picked_up)
 	print("barrell is ready")
 
@@ -23,7 +21,6 @@ func _barrell_explode() -> void:
 	queue_free()
 
 func _get_picked_up(storageVector) -> void:
-	print(storageVector)
 	global_transform.origin = storageVector
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- game-files/Entities/poisen_barrell.gd
+++ game-files/Entities/poisen_barrell.gd
@@ -1,9 +1,9 @@
-class_name Explosive extends Barrell
+class_name Poisen extends Barrell
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("explosive barrell ready")
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
